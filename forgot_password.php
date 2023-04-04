a6530deb211f995633b66dd1c4b3b3ba6dd9af84
Juan <villasmiljuandiego@gmail.com>
2023-04-03 21:52:22 -0400

    ejercicio5

--- forgot_password.php
+++ forgot_password.php
@@ -43,6 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./assets/img/logo.png">
     <title>Document</title>
 </head>
 
